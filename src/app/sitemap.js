7eeb4b0f3e9b76b6210e4604ffa4da5a35b6afa4
Yaaroub Al Haj Dawoud <ydawoud@Mac.fritz.box>
2025-11-22 23:18:42 +0100

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://www.mn-glanzwerk.de/',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1.0,
+      }
+    ];
+  }
+  
